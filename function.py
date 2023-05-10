499a892d685061f9157a54ec7836bb3fae39327c
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-05-10 17:24:44 +0800

    增加时间段的涨跌幅

--- function.py
+++ function.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import easyquotation
+import tushare as ts
 
 def Average(data,ma,length): #输入单个股票的行情数据，返回对应均线数据
     '''
@@ -69,3 +70,31 @@ def cal_pcts(stock_list):#计算一组股票的涨跌幅
     for stock in stock_list:
         pcts.append(cal_pct(stock[2:]))#对tushare股票代码进行处理，保留数字部分
     return pcts
+
+
+def range_pcts(stock_list,start,end):#计算一个时间段内的涨跌幅
+    '''
+    stock_list:一组股票代码
+    start:开始时间:20090101
+    end:结束时间:20090102
+    '''
+    pcts=[]
+    for stock_code in stock_list:
+        df = ts.pro_bar(ts_code=stock_code, adj='qfq', start_date=start, end_date=end)
+        value_end=df['close'][0]#区间终点的股价
+        value_start=df['close'][len(df)-1]#区间起点的股价
+#        print('start',value_start)
+#        print('end',value_end)
+        pct=round((value_end/value_start-1)*100,2)
+        pcts.append(pct)
+    return pcts
+
+
+def get_code(file_path):#提取致富代码
+    with open(file_path,'r') as f:
+        res=f.readlines()#按行读取文件中的内容，每一行为一个字符串，返回以字符串为元素的列表
+        f.close()
+        stock_code=[] #构造空列表，用来存储股票代码
+        for i in res:#遍历所有的结果
+            stock_code.append(i.split('\t')[0])#提取结果中的致富代码
+    return stock_code 

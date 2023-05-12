a0cabdaa118779fdc7e8c83873ffa1cc32e34134
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-05-12 17:07:52 +0800

    增加在线查询数据画图表

--- plot.py
+++ plot.py
@@ -6,7 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import os
-from sqlalchemy import create_engine 
+#from sqlalchemy import create_engine 
+from stock_online import Initial
 
 def plot():
 #    sql="SELECT DISTINCT ts_code, ma"+str(mas)+",close"+" FROM Monthly ORDER BY trade_date DESC;" #构建SQL查询语句
@@ -16,6 +17,22 @@ def plot():
 #    plt.plot(data['trade_date'],data['ma55'])
     plt.show()
 
+def new_plot(stock_code,freq,start,end,ma):#在线查询
+    '''
+    stock_code(str):股票代码
+    freq(str):日，周，月
+    start,end (str): 开始结束时间,如20230510
+    ma (int):均线参数
+    '''
+    pro=Initial()
+    data=ts.pro_bar(ts_code=stock_code,freq=freq,adj='qfq',start_date=start,end_date=end,ma=[ma])
+    data=data.sort_index(axis=0,ascending=False)
+    print(data)
+    plt.plot(data['trade_date'],data[f'ma{ma}'])
+    plt.show()
+    
+
 if __name__ == "__main__":
-    engine_ts = create_engine('mysql://root:administrator@127.0.0.1:3306/stock?charset=utf8&use_unicode=1') ##数据库初始化
-    plot()
+#    engine_ts = create_engine('mysql://root:administrator@127.0.0.1:3306/stock?charset=utf8&use_unicode=1') ##数据库初始化
+#    plot()
+    new_plot('002866.sz','D','20230101','20230511',13)

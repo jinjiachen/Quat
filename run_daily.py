667d243436a2477521c587bfa3bfd0b69bd86226
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-07-18 16:09:36 +0800

    增加日常运行程序

---
+++ run_daily.py
@@ -0,0 +1,12 @@
+#coding=utf8
+
+#import stock_online
+from stock_online import *
+import time
+
+if __name__=='__main__':
+    run_daily()
+#    result=Bottom('D',13,15,2)
+#    filename=f'Dbott13_15_2_{now}.txt'
+#    content=SaveResult(filename,result) #保存结果
+#    notify('post',filename,"".join(content))

2cfa446e6c4a2b803ce341c99f951551d007b893
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-08-15 20:04:27 +0800

    增加定时运行以及自动判断是否更新

--- run_daily.py
+++ run_daily.py
@@ -3,10 +3,17 @@
 #import stock_online
 from stock_online import *
 import time
+from function import is_updated,schedule
+
+
+def daily_combo():
+    now=time.strftime("%Y%m%d") #当前日期
+    pro=Initial()
+    while True:
+        if is_updated(pro,now)=='YES':
+            run_daily()
+            break
+
 
 if __name__=='__main__':
-    run_daily()
-#    result=Bottom('D',13,15,2)
-#    filename=f'Dbott13_15_2_{now}.txt'
-#    content=SaveResult(filename,result) #保存结果
-#    notify('post',filename,"".join(content))
+    schedule(daily_combo,'15:15:00')

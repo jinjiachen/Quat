3c56c3555d8070d011b2f6bdf789b2f8d0a2238f
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-10-19 10:04:20 +0800

    后台运行

--- jq.py
+++ jq.py
@@ -39,7 +39,7 @@ def Driver():
     options = webdriver.ChromeOptions()
 #    options.add_argument("--proxy-server=http://192.168.2.108:8889")
 #    options.add_argument("--no-proxy-server")
-#    options.add_argument("--headless")
+    options.add_argument("--headless")
 #    options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"')
 #    options.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"')
 #    options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
